9db2c851c3c0dbf121372d3697b689bae4d8b039
MuldeR <mulder2@gmx.de>
2019-04-27 16:37:50 +0200

    Updated MediaInfo binaries to v19.04 (2019-04-23).

--- src/Config.h
+++ src/Config.h
@@ -27,17 +27,17 @@
 
 //Version
 MIXP_DEFINE_CONF(unsigned int, mixp_versionMajor,   2)
-MIXP_DEFINE_CONF(unsigned int, mixp_versionMinor,  33)
+MIXP_DEFINE_CONF(unsigned int, mixp_versionMinor,  34)
 
 //MediaInfo Version
-MIXP_DEFINE_CONF(unsigned int, mixp_mediaInfoVerMajor, 18)
-MIXP_DEFINE_CONF(unsigned int, mixp_mediaInfoVerMinor, 12)
+MIXP_DEFINE_CONF(unsigned int, mixp_mediaInfoVerMajor, 19)
+MIXP_DEFINE_CONF(unsigned int, mixp_mediaInfoVerMinor,  4)
 MIXP_DEFINE_CONF(unsigned int, mixp_mediaInfoVerPatch,  0)
 
 //MediaInfo Checksum
-MIXP_DEFINE_CONF(char*, mixp_checksum_gen, "224291ce441fb7521d3870080b47ebd4b86fb6eb4bca4fd5ecdce4a60a865e787ee39d9a737cb70bd0a873e6ba87f6d249531b080517f6699c7d00d73fb983d8")
-MIXP_DEFINE_CONF(char*, mixp_checksum_sse, "0fd4a506eaf950a555fbf14cdb07f27563823839e27d25fddc759b1bd79c350fabcdc2e38ababb88a6d15cd6d1bd164729f26e935b0f6ce938b1f740d5b0c665")
-MIXP_DEFINE_CONF(char*, mixp_checksum_x64, "c070fbcbdcad7e50e416bb28d5ee4cc096902ee8c7099ba8dc83c1b255f42411924fac01c5f1eac9eab35d0f1b02c0643d2ddb936efcd8680dad4b5eeba8a4fa")
+MIXP_DEFINE_CONF(char*, mixp_checksum_gen, "820665985536fbe79dc88ea586d61fcd6d189e6c7444f18339f6515b99ff2e5264b59671237f35d4350e631729abf7fd130563110b449a6d240f342ca6cbf85b")
+MIXP_DEFINE_CONF(char*, mixp_checksum_sse, "8b9cf43c701be3f02241cd8062d484ae6b6fc05aa97725d6a958da7594d7d45bae479a05cb19f5a66ca3d4a6c09ddcc3c94a784991a0ae156206aafc4f725612")
+MIXP_DEFINE_CONF(char*, mixp_checksum_x64, "8819b142a6cb54862576cb1c2af8fc1b117479b57786ef6c8c293f6ba80479910618c2175c4b021dc74ab1e5a338ad7f9c6f19ab985b1ac3b28f10ff1579883a")
 
 //Build date
 MIXP_DEFINE_CONF(char*, mixp_buildDate, __DATE__)
