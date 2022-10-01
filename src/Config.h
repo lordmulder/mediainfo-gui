bce1a299f5346c416dbda33e6c735fc63371995d
MuldeR <mulder2@gmx.de>
2022-10-01 16:03:27 +0200

    Updated MediaInfo binaries to v22.06 (2022-06-23).

--- src/Config.h
+++ src/Config.h
@@ -27,19 +27,19 @@
 
 //Version
 MIXP_DEFINE_CONF(unsigned int, mixp_versionMajor,  2)
-MIXP_DEFINE_CONF(unsigned int, mixp_versionMinor, 41)
+MIXP_DEFINE_CONF(unsigned int, mixp_versionMinor, 42)
 MIXP_DEFINE_CONF(unsigned int, mixp_versionPatch,  0)
 
 //MediaInfo Version
 MIXP_DEFINE_CONF(unsigned int, mixp_mediaInfoVerMajor, 22)
-MIXP_DEFINE_CONF(unsigned int, mixp_mediaInfoVerMinor,  3)
+MIXP_DEFINE_CONF(unsigned int, mixp_mediaInfoVerMinor,  6)
 MIXP_DEFINE_CONF(unsigned int, mixp_mediaInfoVerPatch,  0)
 
 //MediaInfo Checksum
-MIXP_DEFINE_CONF(char*, mixp_checksum_x86_i686, "248e9e0211290e317e4384fe45043a42ecba20cf6b02c0216f821cf7d2515623d931beabb6a882a2c3ad6e1d08bec65015bf7bf00630b72f5e117ca0a4755256")
-MIXP_DEFINE_CONF(char*, mixp_checksum_x86_sse2, "03fd624a566e13b70d35b8b109da8e0378faeab44c7de8a221128dcdddcdbd1e446a2e073a15cff9a667264ba88a928b74c3e1b2c33153530f066337b6f6246f")
-MIXP_DEFINE_CONF(char*, mixp_checksum_x64_sse2, "84b0d58dfcd93e4d0dcf85e2b3a91e80d5194dd0a680996a4c625509e0befe0499b5a41557e594d32dcfdb54ebac6430b443c6dd29b95133ae9d50f9e684f43e")
-MIXP_DEFINE_CONF(char*, mixp_checksum_x64_avx2, "8261d2ed869bc434e3b20b6a75d457ab677e9fcc93652c09853fc0993703e47cd3e9713084816cf1d6de3800ba1a0cda5679983b347ca42fccb4c163fdadea7d")
+MIXP_DEFINE_CONF(char*, mixp_checksum_x86_i686, "698cf0dbe2b6fcf2b56d4c77e041b4f5dbb4fc39f20d2ae7742dfde02c7669c1051034c2643aa99886d64f4140d28ace73aa7716b4f27b0a787f8774f452638d")
+MIXP_DEFINE_CONF(char*, mixp_checksum_x86_sse2, "1878d907ec130e0a5962a6c11ddd2ca7431bc3d13ad70475fe2c22376608199d9d029f8f52772970897dd7119d9eeca19a277195dac6fb83e3fa0b8809786e04")
+MIXP_DEFINE_CONF(char*, mixp_checksum_x64_sse2, "a61ac0daccba8dd1795906d9ec459fb42e0b242d98302bc2a77767f4eabf5c67badcda1fd11e790db4fe35f0d33445e2c4f6ef0dc702f59ad423640841ddfb03")
+MIXP_DEFINE_CONF(char*, mixp_checksum_x64_avx2, "22482f01075025171d841592caf52ec6109b600f6e9d635201f0b25b9c3ee03dec6ebab7544b4d5ea3bc3ef84f367f791b92a5702fb6e2797f8a451c8c65194a")
 
 //Build date
 MIXP_DEFINE_CONF(char*, mixp_buildDate, __DATE__)
